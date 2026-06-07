4380f59fb4dfd93e7ef4ef422a18a1666a24d321
Matias <pmg95mdq@gmail.com>
2026-06-06 23:54:37 -0300

    problema de merge

--- PetCare/src/main/java/com/example/PetCare/repository/UsuarioRepository.java
+++ PetCare/src/main/java/com/example/PetCare/repository/UsuarioRepository.java
@@ -1,13 +1,19 @@
 package com.example.PetCare.repository;
 
+import com.example.PetCare.enums.Rol;
 import com.example.PetCare.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
-    // Le agregue findByEmail que lo usa el AuthUtils
-    Optional<Usuario> findByEmail(String email);
+    Optional<Usuario> findByEmail(String email); // lo usa AuthUtils
+    List<Usuario> findByRol(Rol role);
+    List<Usuario> findByNombre(String nombre);
+    List<Usuario> findByTelefono(String telefono);
+    List<Usuario> findByDireccion(String direccion);
+    List<Usuario> findByActivo(boolean activo);
 }
